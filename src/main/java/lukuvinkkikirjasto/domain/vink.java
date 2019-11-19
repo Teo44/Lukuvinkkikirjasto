f08faabf62f80aa44c3fa1c637aa0d20affcda61
LKonsta <komppa.lehto@gmail.com>
2019-11-19 17:10:31 +0200

    created vink object

---
+++ src/main/java/lukuvinkkikirjasto/domain/vink.java
@@ -0,0 +1,52 @@
+package lukuvinkkikirjasto.domain;
+
+import java.util.ArrayList;
+
+public class vink {
+
+    private String headline;
+    private String type;
+    private ArrayList<String> tags;
+    private String conmment;
+
+    public vink(String headline, String type, ArrayList<String> tags, String comment) {
+        this.headline = headline;
+        this.type = type;
+        this.tags = tags;
+        this.conmment = comment;   
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public ArrayList getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList tags) {
+        this.tags = tags;
+    }
+
+    public String getConmment() {
+        return conmment;
+    }
+
+    public void setConmment(String conmment) {
+        this.conmment = conmment;
+    }
+    
+    
+}
