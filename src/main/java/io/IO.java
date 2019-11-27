57c1f870b71e715f64f665425bf4343e185a42a2
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-11-27 12:13:13 +0200

    Refactor TextualUI to use IO interface

--- src/main/java/io/IO.java
+++ src/main/java/io/IO.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io;
 
-/**
- *
- * @author khlehto
- */
-interface IO {
+public interface IO {
     void print(String toPrint);
-    int readInt(String prompt);
-    String readLine(String prompt);
+    public String askUser(String question);
+    public String readLine(String prompt);
 }
