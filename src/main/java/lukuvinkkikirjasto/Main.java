ee8875224cb4a91ea51b06c8e6118dc7157b24a3
henripalin <henri.j.palin@gmail.com>
2019-11-27 11:10:42 +0200

    added vinkDao connection to Main

--- src/main/java/lukuvinkkikirjasto/Main.java
+++ src/main/java/lukuvinkkikirjasto/Main.java
@@ -3,10 +3,12 @@ package lukuvinkkikirjasto;
 import java.util.Scanner;
 import logic.Logic;
 import ui.Textual;
+import database.VinkDAO;
 
 public class Main {
 
     public static void main(String[] args) {
-        new Textual(new Scanner(System.in), new Logic()).run();
+        VinkDAO vinkDao = new VinkDAO("vinkDatabase.db"); 
+        new Textual(new Scanner(System.in), new Logic(vinkDao)).run();
     }
 }
