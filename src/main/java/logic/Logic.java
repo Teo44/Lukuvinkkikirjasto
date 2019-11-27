a57679acec8c453c966309c4a729eca39a518a31
henripalin <henri.j.palin@gmail.com>
2019-11-27 10:52:44 +0200

    added deleteDuplicates method

--- src/main/java/logic/Logic.java
+++ src/main/java/logic/Logic.java
@@ -4,6 +4,8 @@ import domain.Vink;
 import database.VinkDAO;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Logic {
     private VinkDAO vinkDao;
@@ -13,13 +15,24 @@ public class Logic {
         vinkDao.createTablesIfNotExist();
     }
     
-    public void saveVink(String headline, String type, ArrayList<String> tags, String comment) {
+    public boolean saveVink(String headline, String type, ArrayList<String> tags, String comment) {
         Vink vink = new Vink(headline, type, tags, comment);
+        
         vinkDao.addVink(vink);
+        return true;                        //returns true if the saving was successfull
+                                            //logic to be implemented later
     }
     
     public ArrayList<Vink> getAllVinks() {
         return vinkDao.getAllVinks();
+    }
+    
+    public ArrayList<String> deleteDuplicates(ArrayList<String> list) {
+        ArrayList<String> newList = list;
+        Set<String> set = new HashSet<>(newList);
+        newList.clear();
+        newList.addAll(set);
+        return newList;
     }
     
     
