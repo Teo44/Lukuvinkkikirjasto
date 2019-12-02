5f8faf59cd7a8dc5d4f303cbf4f52b3779f21c89
henripalin <henri.j.palin@gmail.com>
2019-12-02 16:03:14 +0200

    added colorised filtering

--- src/main/java/logic/Logic.java
+++ src/main/java/logic/Logic.java
@@ -38,11 +38,8 @@ public class Logic {
     
     public boolean updateVink(Integer id, String headline, String type, ArrayList<String> tags, String comment, String link) {
         Vink vink = new Vink(headline, type, tags, comment, link, id);
-        boolean successful = vinkDao.updateVink(vink);
-        if (successful) {
-            return true;
-        }
-        return false;
+        
+        return vinkDao.updateVink(vink);
     }
     
 
@@ -59,7 +56,7 @@ public class Logic {
     
     public ArrayList<Vink> filterByString(String string) {
         ArrayList<Vink> vinkList = getAllVinks();
-        return filter.filterByString(vinkList, string);
+        return filter.getFilteredColorisedList(vinkList, string);
     }
     
     private ArrayList<String> deleteDuplicates(ArrayList<String> list) {
