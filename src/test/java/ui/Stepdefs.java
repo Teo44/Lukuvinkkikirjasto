0ca160aa56bc5ea1759186927ae489b34b85fa56
LKonsta <komppa.lehto@gmail.com>
2019-12-02 15:24:15 +0200

    new failing cucumber test added

--- src/test/java/ui/Stepdefs.java
+++ src/test/java/ui/Stepdefs.java
@@ -52,6 +52,16 @@ public class Stepdefs {
     public void commandModifyIsSelected() {
         inputLines.add("modify");
     }
+    
+    @Given("^command nonexistant is selected")
+    public void commandNonexistantIsSelected() {
+        inputLines.add("nonexistant");
+        inputLines.add("quit");
+                
+        io = new StubIO(inputLines);
+        text = new Textual(logic, io);
+        text.run();  
+    }
 
     @When("headline {string} and type {string} and tags {string} and comment {string} and link {string} is selected")
     public void newVinkInfoEntered(String headline, String type, String tags, String comment, String link) {
--- src/test/resources/ui/creating_vinks.feature
+++ src/test/resources/ui/creating_vinks.feature
@@ -22,3 +22,12 @@ Feature: User can create vinks
         When  headline "Testi" is selected
         Then  system will respond with "Vink deleted"
 
+
+    Scenario: user cannot delete a vink that doesn,t exist
+        Given command delete is selected
+        When  headline "Olematon" is selected
+        Then  system will respond with "Something went wrong when trying to delete vink, try again!"
+
+    Scenario: user cannot use un supported commands
+        Given command nonexistant is selected
+        Then  system will respond with "Unrecognized command" 
