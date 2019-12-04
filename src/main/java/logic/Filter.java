293bea31eb258cf1b5f0d04f1f6f2789469ebcac
henripalin <henri.j.palin@gmail.com>
2019-12-04 12:56:37 +0200

    fixed bug in filtering

--- src/main/java/logic/Filter.java
+++ src/main/java/logic/Filter.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Filter {
 
     private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_YELLOW = "\u001b[32;1m";
+    private static final String ANSI_GREEN = "\u001b[32;1m";
 
     public ArrayList<Vink> getFilteredColorisedList(ArrayList<Vink> vinks, String string) {
         ArrayList<Vink> filteredList = new ArrayList<>();
@@ -15,35 +15,34 @@ public class Filter {
             boolean modified = false;
 
             if (vinks.get(i).getHeadline().contains(string)) {
-                String colorHeadline = vinks.get(i).getHeadline().replaceAll(string, ANSI_YELLOW + string + ANSI_RESET);
+                String colorHeadline = vinks.get(i).getHeadline().replaceAll(string, ANSI_GREEN + string + ANSI_RESET);
                 vinks.get(i).setHeadline(colorHeadline);
                 modified = true;
             }
 
             if (vinks.get(i).getType().contains(string)) {
-                String colorType = vinks.get(i).getType().replaceAll(string, ANSI_YELLOW + string + ANSI_RESET);
-                vinks.get(i).setHeadline(colorType);
+                String colorType = vinks.get(i).getType().replaceAll(string, ANSI_GREEN + string + ANSI_RESET);
+                vinks.get(i).setType(colorType);
                 modified = true;
             }
 
             if (vinks.get(i).getComment().contains(string)) {
-                String colorComment = vinks.get(i).getHeadline().replaceAll(string, ANSI_YELLOW + string + ANSI_RESET);
+                String colorComment = vinks.get(i).getComment().replaceAll(string, ANSI_GREEN + string + ANSI_RESET);
                 vinks.get(i).setComment(colorComment);
                 modified = true;
             }
 
             if (vinks.get(i).getLink().contains(string)) {
-                String colorLink = vinks.get(i).getLink().replaceAll(string, ANSI_YELLOW + string + ANSI_RESET);
+                String colorLink = vinks.get(i).getLink().replaceAll(string, ANSI_GREEN + string + ANSI_RESET);
                 vinks.get(i).setLink(colorLink);
                 modified = true;
             }
             
             ArrayList<String> tags = vinks.get(i).getTags();
-            ArrayList<String> coloredTags = new ArrayList<>();
 
             for (int j = 0; j < tags.size(); j++) {
                 if (tags.get(j).contains(string)) {
-                    String colorTag = tags.get(j).replaceAll(string, ANSI_YELLOW + string + ANSI_RESET);
+                    String colorTag = tags.get(j).replaceAll(string, ANSI_GREEN + string + ANSI_RESET);
                     tags.set(j, colorTag);
                     modified = true;
                 }
--- src/main/java/ui/FilterVinks.java
+++ src/main/java/ui/FilterVinks.java
@@ -1,6 +1,8 @@
 package ui;
 
+import domain.Vink;
 import io.IO;
+import java.util.ArrayList;
 import logic.Logic;
 
 public class FilterVinks extends Command {
@@ -11,28 +13,49 @@ public class FilterVinks extends Command {
 
     @Override
     public void handleCommand() {
-        String keyword = io.askUser("Filter by keyword");
-
-        io.print("Filtered vinks:");
-
-        logic.filterByString(keyword).forEach(v -> {
-            io.print("------------------------------");
-            io.print("Headline: " + v.getHeadline());
-            io.print("Type: " + v.getType());
-            String tagString = new String();
-            for (int i = 0; i < v.getTags().size(); i++) {
-                tagString = tagString + v.getTags().get(i);
-                if (i+1 != v.getTags().size()) {
-                    tagString = tagString + ", ";
-                }
-            }
-            io.print("Tags: " + tagString);
-            io.print("Comment: " + v.getComment());
-            if (!(v.getLink().equals("")))   {
-                io.print("Link: " + v.getLink());
-            }
-        });
+        String keyword = "";
         
+        while (true) {
+            keyword = io.askUser("Filter by keyword");
+            
+            if (keyword.trim().isEmpty()) {
+                io.print("Keyword must not be empty or contain only spaces!");
+                io.print("");
+            } else {
+                break;
+            }
+        }
+        
+        ArrayList<Vink> filteredList = logic.filterByString(keyword);
+
+        if (!filteredList.isEmpty()) {
+            io.print("Found " + filteredList.size() + " vink(s) including keyword " + keyword + ":");
+
+            filteredList.forEach(v -> {
+                io.print("------------------------------");
+                io.print("Headline: " + v.getHeadline());
+                io.print("Type: " + v.getType());
+                String tagString = new String();
+                
+                for (int i = 0; i < v.getTags().size(); i++) {
+                    tagString = tagString + v.getTags().get(i);
+                    if (i + 1 != v.getTags().size()) {
+                        tagString = tagString + ", ";
+                    }
+                }
+                
+                io.print("Tags: " + tagString);
+                io.print("Comment: " + v.getComment());
+                
+                if (!(v.getLink().equals(""))) {
+                    io.print("Link: " + v.getLink());
+                }
+            });
+            
+        } else {
+            io.print("No matches found with keyword " + keyword);
+        }
+
     }
-    
+
 }
