4291a5c6501db5579ed1456d64fe0b76160ab778
henripalin <henri.j.palin@gmail.com>
2019-12-07 19:24:32 +0200

    added progress bar to filter UI

--- src/main/java/ui/FilterVinks.java
+++ src/main/java/ui/FilterVinks.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import logic.Logic;
 
 public class FilterVinks extends Command {
+    ProgressBar progressBar = new ProgressBar();
 
     public FilterVinks(IO io, Logic logic) {
         super(io, logic);
@@ -14,10 +15,10 @@ public class FilterVinks extends Command {
     @Override
     public void handleCommand() {
         String keyword = "";
-        
+
         while (true) {
             keyword = io.askUser("Filter by keyword");
-            
+
             if (keyword.trim().isEmpty()) {
                 io.print("Keyword must not be empty or contain only spaces!");
                 io.print("");
@@ -25,7 +26,7 @@ public class FilterVinks extends Command {
                 break;
             }
         }
-        
+
         ArrayList<Vink> filteredList = logic.filterByString(keyword);
 
         if (!filteredList.isEmpty()) {
@@ -33,28 +34,37 @@ public class FilterVinks extends Command {
 
             filteredList.forEach(v -> {
                 io.print("------------------------------");
+
+                if (v.getReadingStatus().equals(1)) {
+                    io.print(progressBar.getProgressBar(1));
+                } else if (v.getReadingStatus().equals(2)) {
+                    io.print(progressBar.getProgressBar(2));
+                } else if (v.getReadingStatus().equals(3)) {
+                    io.print(progressBar.getProgressBar(3));
+                }
+
                 io.print("Headline: " + v.getHeadline());
                 io.print("Type: " + v.getType());
-                if (!(v.getAuthor().equals("")))    {
+                if (!(v.getAuthor().equals(""))) {
                     io.print("Author: " + v.getAuthor());
                 }
                 String tagString = new String();
-                
+
                 for (int i = 0; i < v.getTags().size(); i++) {
                     tagString = tagString + v.getTags().get(i);
                     if (i + 1 != v.getTags().size()) {
                         tagString = tagString + ", ";
                     }
                 }
-                
+
                 io.print("Tags: " + tagString);
                 io.print("Comment: " + v.getComment());
-                
+
                 if (!(v.getLink().equals(""))) {
                     io.print("Link: " + v.getLink());
                 }
             });
-            
+
         } else {
             io.print("No matches found with keyword " + keyword);
         }
