d9fbaad3514c4e1648310906ec02252d52bb0e12
Milla <milla.kortelainen@helsinki.fi>
2019-12-09 23:12:10 +0200

    muutettiin muuttujan nimi

--- src/main/java/logic/Filter.java
+++ src/main/java/logic/Filter.java
@@ -8,38 +8,38 @@ public class Filter {
     private static final String ANSI_RESET = "\u001B[0m";
     private static final String ANSI_GREEN = "\u001b[32;1m";
 
-    public ArrayList<Vink> getFilteredColorisedList(ArrayList<Vink> vinks, String string) {
+    public ArrayList<Vink> getFilteredColorisedList(ArrayList<Vink> vinks, String merkkijono) {
         ArrayList<Vink> filteredList = new ArrayList<>();
 
         for (int i = 0; i < vinks.size(); i++) {
             boolean modified = false;
 
-            if (vinks.get(i).getHeadline().contains(string)) {
-                String colorHeadline = vinks.get(i).getHeadline().replaceAll(string, ANSI_GREEN + string + ANSI_RESET);
+            if (vinks.get(i).getHeadline().contains(merkkijono)) {
+                String colorHeadline = vinks.get(i).getHeadline().replaceAll(merkkijono, ANSI_GREEN + merkkijono + ANSI_RESET);
                 vinks.get(i).setHeadline(colorHeadline);
                 modified = true;
             }
             
-            if (vinks.get(i).getAuthor().contains(string))  {
-                String colorAuthor = vinks.get(i).getAuthor().replaceAll(string, ANSI_GREEN + string + ANSI_RESET);
+            if (vinks.get(i).getAuthor().contains(merkkijono))  {
+                String colorAuthor = vinks.get(i).getAuthor().replaceAll(merkkijono, ANSI_GREEN + merkkijono + ANSI_RESET);
                 vinks.get(i).setAuthor(colorAuthor);
                 modified = true;
             }
 
-            if (vinks.get(i).getType().contains(string)) {
-                String colorType = vinks.get(i).getType().replaceAll(string, ANSI_GREEN + string + ANSI_RESET);
+            if (vinks.get(i).getType().contains(merkkijono)) {
+                String colorType = vinks.get(i).getType().replaceAll(merkkijono, ANSI_GREEN + merkkijono + ANSI_RESET);
                 vinks.get(i).setType(colorType);
                 modified = true;
             }
 
-            if (vinks.get(i).getComment().contains(string)) {
-                String colorComment = vinks.get(i).getComment().replaceAll(string, ANSI_GREEN + string + ANSI_RESET);
+            if (vinks.get(i).getComment().contains(merkkijono)) {
+                String colorComment = vinks.get(i).getComment().replaceAll(merkkijono, ANSI_GREEN + merkkijono + ANSI_RESET);
                 vinks.get(i).setComment(colorComment);
                 modified = true;
             }
 
-            if (vinks.get(i).getLink().contains(string)) {
-                String colorLink = vinks.get(i).getLink().replaceAll(string, ANSI_GREEN + string + ANSI_RESET);
+            if (vinks.get(i).getLink().contains(merkkijono)) {
+                String colorLink = vinks.get(i).getLink().replaceAll(merkkijono, ANSI_GREEN + merkkijono + ANSI_RESET);
                 vinks.get(i).setLink(colorLink);
                 modified = true;
             }
@@ -47,8 +47,8 @@ public class Filter {
             ArrayList<String> tags = vinks.get(i).getTags();
 
             for (int j = 0; j < tags.size(); j++) {
-                if (tags.get(j).contains(string)) {
-                    String colorTag = tags.get(j).replaceAll(string, ANSI_GREEN + string + ANSI_RESET);
+                if (tags.get(j).contains(merkkijono)) {
+                    String colorTag = tags.get(j).replaceAll(merkkijono, ANSI_GREEN + merkkijono + ANSI_RESET);
                     tags.set(j, colorTag);
                     modified = true;
                 }
